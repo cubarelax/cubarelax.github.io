af74d8fe385847ee0ffd3156994b3bb7f4fc6f74
cubarelax <70276645+cubarelax@users.noreply.github.com>
2020-08-26 17:28:22 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+  $(".photo__slider").slick({
+    /* arrows: false, */
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/arrow_left.svg" alt=""></button>',
+    nextArrow:
+      '<button type ="button" class="slick-next"><img src="images/arrow_right.svg" alt=""></button>',
+  });
+});
